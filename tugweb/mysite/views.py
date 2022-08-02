b7c2f015e7cfa7809f0423aae4d2314e115bd9d4
zxc21415 <u0424008@nkfust.edu.tw>
2022-08-02 11:59:53 +0800

    頁面架構(有哪些)大概可以，完成介紹的頁面設計，介紹文案還沒打、ICON還沒換

--- tugweb/mysite/views.py
+++ tugweb/mysite/views.py
@@ -3,11 +3,12 @@ import plotly.graph_objs as go
 
 # Create your views here.
 def index(request):
-
     return render(request,"index.html",locals())
 
 def chart(request):
-    
-   
-
+    url = 'chart'
     return render(request,"chart.html",locals())
+
+def introduce(request):
+    url = 'introduce'
+    return render(request,"introduce.html",locals())
